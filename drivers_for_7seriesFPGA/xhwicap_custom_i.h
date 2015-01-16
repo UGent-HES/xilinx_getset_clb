78dcedaa58258228ade48d01c2c530bb6c25b580
Amit <Amit.Kulkarni@Ugent.be>
2015-01-16 13:34:05 +0100

    Added the disclaimer notice from Xilinx Inc.

--- drivers_for_7seriesFPGA/xhwicap_custom_i.h
+++ drivers_for_7seriesFPGA/xhwicap_custom_i.h
@@ -1,3 +1,16 @@
+/******************************************************************************************************
+DISCLAIMER NOTICE
+*******************************************************************************************************
+We are not affiliated, associated, authorized, endorsed by, 
+or in any way officially connected with Xilinx, Inc. 
+or any of its subsidiaries or its affiliates.  
+In no event whatsoever shall Xilinx, Inc. 
+or any of its subsidiaries or its affiliates have any warranty or support commitment for this software 
+or liability for loss, injury or damage in connection with this software, 
+including but not limited to the use or display thereof.
+*******************************************************************************************************
+*Created by Ghent University.
+*/
 #ifndef XHWICAP_CUSTOM_I_H_ /* prevent circular inclusions */
 #define XHWICAP_CUSTOM_I_H_ /* by using protection macros */
 
